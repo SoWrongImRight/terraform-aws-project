49c8295664d993169e4e018b002cf858dd55f2e3
Russ Carroll <russ.carroll@pm.me>
2021-07-14 10:54:10 -0400

    Added instance and subnet groups for the database.

--- main.tf
+++ main.tf
@@ -272,3 +272,27 @@ resource "aws_lb_listener" "external-elb" {
     target_group_arn = aws_lb_target_group.external-elb.arn
   }
 }
+
+# Create DB Subnet group
+resource "aws_db_instance" "default" {
+  allocated_storage = 10
+  db_subnet_group_name = aws_db_subnet_group.default.id
+  engine = "mysql"
+  engine_version = "8.0.20"
+  instance_class = "db.t2.micor"
+  multi_az = true
+  name = "mydb"
+  username = "TestUser"
+  password = "TestPassword"
+  skip_final_snapshot = true
+  vpc_security_group_ids = [aws_security_group.database-sg.id]
+}
+
+resource "aws_db_subnet_group" "default" {
+  name = "main"
+  subnet_ids = [aws_subnet.database-subnet-1.id, aws_subnet.database-subnet-2.id]
+
+  tags = {
+      Name = "My DB subnet group"
+  }
+}
