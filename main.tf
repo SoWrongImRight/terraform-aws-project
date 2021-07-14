27992f548eb406f57543b246c13197085ffbdbde
Russ Carroll <russ.carroll@pm.me>
2021-07-14 09:18:31 -0400

    Setup route IGW, route table, and associations.

--- main.tf
+++ main.tf
@@ -87,3 +87,37 @@ resource "aws_subnet" "database-subnet-2" {
       Name = "Database-2b"
   }
 }
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.my-vpc.id
+
+    tags = {
+        Name = "Demo IGW"
+    }  
+}
+
+# Create Web layer route table
+resource "aws_route_table" "web-rt" {
+  vpc_id = aws_vpc.my-vpc.id
+
+  route = {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+      Name = "WebRT"
+  }
+}
+
+# Create Web Subnet association with route table
+resource "aws_route_table_association" "a" {
+  subnet_id = aws_subnet.web-subnet-1.id
+  route_table_id = aws_route_table.web-rt.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id = aws_subnet.web-subnet-2.id
+  route_table_id = aws_route_table.web-rt.id
+}
