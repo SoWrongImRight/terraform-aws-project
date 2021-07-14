8e05ba4f4cf6d74293135d05600e67b916a7806b
Russ Carroll <russ.carroll@pm.me>
2021-07-14 10:48:47 -0400

    Added the load balancer configurations.

--- main.tf
+++ main.tf
@@ -136,7 +136,7 @@ resource "aws_instance" "webserver1" {
   }
 }
 
-resource "aws_instance" "webserver1" {
+resource "aws_instance" "webserver2" {
   ami = "ami-0d5eff06f840b45e9"
   instance_type = "t2.micro"
   availability_zone = "us-east-1b"
@@ -224,5 +224,51 @@ resource "aws_security_group" "database-sg" {
 
   tags = {
       Name = "Database-SG"
+  }
+}
+
+resource "aws_lb" "external-elb" {
+  name = "External-LB"
+  internal = false
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.web-sg.id]
+  subnets = [aws_subnet.web-subnet-1.id, aws_subnet.web-subnet-2.id]
+}
+
+resource "aws_lb_target_group" "external-elb" {
+  name = "ALB-TG"
+  port = 80
+  protocol = "tcp"
+  vpc_id = aws_vpc.my-vpc.id
+}
+
+resource "aws_lb_target_group_attachment" "external-elb1" {
+  target_group_arn = aws_lb_target_group.external-elb.arn
+  target_id = aws_instance.webserver1.id
+  port = 80
+
+  depends_on = [
+    aws_instance.webserver1,
+  ]
+}
+
+resource "aws_lb_target_group_attachment" "external-elb2" {
+  target_group_arn = aws_lb_target_group.external-elb.arn
+  target_id = aws_instance.webserver2.id
+  port = 80
+
+  depends_on = [
+    aws_instance.webserver2,
+  ]
+}
+
+resource "aws_lb_listener" "external-elb" {
+  load_balancer_arn = aws_lb.external-elb.arn
+  port = 80
+  protocol = "tcp"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.external-elb.arn
   }
 }
